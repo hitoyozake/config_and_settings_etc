e535e7b72324945c08967239139a172f57ff01c6
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-09-05 00:36:44 +0900

    zsh auto suggestion added

---
+++ ubuntu_settings/install_zsh_auto_suggestion.bash
@@ -0,0 +1,8 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# mac 
+# brew install zsh-autosuggestions
+# echo source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zs h >> ~/.zshrc
+
